7ec165f3c744f7a179aa0d65ed55ce0896ab87d2
JayDo26 <dokiet2602@gmail.com>
2025-10-19 18:17:47 +0700

    Fix GitHub Pages routing - add basename and SPA redirect handling

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import ScrollToTop from './components/ScrollToTop';
 function App() {
   return (
     
-      <Router>
+      <Router basename="/CalnovaAI">
           <GlobalStyles />
           <ScrollToTop />
           <Navbar />
